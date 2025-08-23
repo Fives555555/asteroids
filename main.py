da527cdb1a36c5fe6a2eb34ce863b27cb164820a
Fives555555 <108680435+Fives555555@users.noreply.github.com>
2025-08-23 16:45:35 +0100

    use groups for player and other future items for clean code

--- main.py
+++ main.py
@@ -8,22 +8,33 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     # Clock and delta t
     clock = pygame.time.Clock()
-    dt = 0
+    
+    # Groups
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    
+    Player.containers = (updatable, drawable)
+    
     # Initialise player
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+    
+    dt = 0
     
     while True:
         # Quit condition
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
         # Update player's position from inputs
-        player.update(dt)
+        updatable.update(dt)
         
         # Draw black background
         screen.fill("black")
-        # Draw player
-        player.draw(screen)
+        # Draw items to screen (player, asteroids, shots)
+        for item in drawable:
+            item.draw(screen)
+            
         pygame.display.flip()
         
         #Limit framerate to 60 FPS
